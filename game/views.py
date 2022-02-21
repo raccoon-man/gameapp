9e25d30694e1973cba43ddaaec495b039a7f0fcf
A queen <1139051802@qq.com>
2022-02-21 11:17:57 +0800

    compress js

--- game/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style = "text-align: center">云顶之奕</h1>'
-    line2 = '<img src = "https://pic.netbian.com/uploads/allimg/220208/200740-1644322060cc71.jpg">'
-    return HttpResponse(line1 + line2)

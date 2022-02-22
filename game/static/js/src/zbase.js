e862de4e6f29fc843082777eceef93603e0b6529
A queen <1139051802@qq.com>
2022-02-22 16:32:40 +0800

    game completed

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,9 +1,9 @@
-class SpxGame{
+export class SpxGame{
     constructor(id){
         this.id = id;
         console.log("Zbase");
         this.$spx_game = $('#'+id);
-        this.menu = new SpxGameMenu(this);
+        //this.menu = new SpxGameMenu(this);
         this.playground = new SpxGamePlayground(this);
 
         this.start();
